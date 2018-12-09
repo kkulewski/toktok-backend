cb5ca881f79be64acdddf525b27ce5789b533ba7
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-12-09 23:03:15 +0100

    Add SqliteDbContext

---
+++ TokTok/Data/TokTokDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TokTok.Models;
+
+namespace TokTok.Data
+{
+    public class TokTokDbContext : DbContext
+    {
+        public TokTokDbContext()
+        {
+        }
+
+        public TokTokDbContext(DbContextOptions<TokTokDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=toktok.db");
+        }
+    }
+}
